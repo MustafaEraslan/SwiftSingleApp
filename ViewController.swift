0bb2618a8ff7aa4c7fab97f1796ccb57739cca02
MustafaEraslan <eraslan.mustafa@outlook.com>
2022-05-29 13:57:31 +0300

    update

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftSingleApp
+//
+//  Created by Support on 29.05.2022.
+//  Copyright © 2022 Support. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
